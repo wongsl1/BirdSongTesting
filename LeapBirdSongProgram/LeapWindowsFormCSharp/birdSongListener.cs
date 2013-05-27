fca4b63a6e1b1506eb80baa9b2e88c8984f3350d
Sammy Wong <sammywong@Sammys-MacBook-Pro.local>
2013-05-27 13:57:41 +1200

    LeapITC#

---
+++ LeapBirdSongProgram/LeapWindowsFormCSharp/birdSongListener.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Leap;
+using System.Media;
+
+namespace LeapWindowsFormCSharp
+{
+    class birdSongListener : Listener
+    {
+        private delegate void writeLineDel(string line);
+
+        private Object thisLock;
+        private SoundPlayer soundPlayer;
+
+        public birdSongListener(SoundPlayer soundPlayer)
+        {
+            this.soundPlayer = soundPlayer;
+            thisLock = new Object();
+        }
+
+        public void lineToBox(string line)
+        {
+            //displayBox.Items.Add(line);
+        }
+
+
+        private void SafeWriteLine(String line)
+        {
+            lock (thisLock)
+            {
+                writeLineDel writeLineDel = new writeLineDel(lineToBox);
+                //displayBox.Invoke(writeLineDel, line);
+            }
+        }
+
+        public override void OnInit(Controller controller)
+        {
+            //SafeWriteLine("OnInit Raised");
+        }
+
+        public override void OnConnect(Controller controller)
+        {
+            //SafeWriteLine("OnConnect Raised");
+            controller.EnableGesture(Gesture.GestureType.TYPECIRCLE);
+            controller.EnableGesture(Gesture.GestureType.TYPEKEYTAP);
+            controller.EnableGesture(Gesture.GestureType.TYPESCREENTAP);
+            controller.EnableGesture(Gesture.GestureType.TYPESWIPE);
+        }
+
+        public override void OnDisconnect(Controller controller)
+        {
+            //SafeWriteLine("OnDisconnect Raised");
+        }
+
+        public override void OnExit(Controller controller)
+        {
+            //SafeWriteLine("On Exit Raised");
+        }
+
+        public override void OnFrame(Controller controller)
+        {
+            // Get the most recent frame and report some basic information
+            Frame frame = controller.Frame();
+
+            // Get gestures
+            GestureList gestures = frame.Gestures();
+            for (int i = 0; i < gestures.Count; i++)
+            {
+                Gesture gesture = gestures[i];
+
+                switch (gesture.Type)
+                {
+                    case Gesture.GestureType.TYPEKEYTAP:
+                        soundPlayer.Play();
+                        break;
+                    case Gesture.GestureType.TYPESCREENTAP:
+                        soundPlayer.Stop();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ LeapBirdSongProgramTEST/LeapWindowsFormCSharp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Leap;
+using System.Media;
+
+//NB:
+// Add a reference to LeapCSharp.NET4.0.dll to the project, but make sure that Leap.dll, 
+// LeapCSharp.dll & LeapCSharp.NET4.0.dll are all in the Debug folder. Must match machine configuration
+// as well.
+
+
+namespace LeapWindowsFormCSharp
+{
+    public partial class Form1 : Form
+    {
+
+        SoundPlayer player;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player = new SoundPlayer("BirdSong.wav");
+
+            // Create a sample listener and controller
+            SampleListener listener = new SampleListener(listBox1);
+            Controller controller = new Controller();
+
+            // Have the sample listener receive events from the controller
+            controller.AddListener(listener);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.PlayLooping();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+    }
+}
